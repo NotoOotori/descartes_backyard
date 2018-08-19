542e1f1efa46bd837dcc87b8000576881d6b7dc4
NotoOotori <345238563@qq.com>
2018-08-19 18:37:49 +0900

    Merge keys.py and key.py.

--- python/houtou_project/keys.py
+++ python/houtou_project/keys.py
@@ -1,6 +1,16 @@
 """ The class Keys"""
-from key import Key
+class Key():
+    """ Store the status of a key."""
+    def __init__(self):
+        self.hold = False
 
+    def press(self):
+        """ Record the key press."""
+        self.hold = True
+
+    def release(self):
+        """ Record the key release."""
+        self.hold = False
 
 class Keys():
     """ Store the status of keys."""
