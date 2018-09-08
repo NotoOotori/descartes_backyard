f687153b446a825e5d0c313ec0893045257f4659
NotoOotori <345238563@qq.com>
2018-09-08 16:23:36 +0900

    Add a return.

--- python/houtou_project/enemy_repository.py
+++ python/houtou_project/enemy_repository.py
@@ -5,4 +5,5 @@ from bat_girl import BatGirl
 class EnemyRepo():
     """ Store instances of all enemies."""
     def __init__(self, screen):
-        self.bat_girl = BatGirl(screen, 'images/player.png', (screen.get_rect().centerx, 60), 150000)
+        self.bat_girl = BatGirl(screen, 'images/player.png',
+                                (screen.get_rect().centerx, 60), 150000)
